13616c469ba01b2512e37bdce731027042fe5dee
Alexander Morales Panitz <alexander.morales@utec.edu.pe>
2022-06-11 14:24:23 -0500

    FCFS implemented

---
+++ driver.c
@@ -0,0 +1,61 @@
+/**
+ * Driver.c
+ *
+ * Schedule is in the format
+ *
+ *  [name] [priority] [CPU burst]
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "task.h"
+#include "list.h"
+#include "schedulers.h"
+
+#define SIZE    100
+
+char *strsep(char **stringp, const char *delim) {
+    // Reference: https://stackoverflow.com/questions/58244300/getting-the-error-undefined-reference-to-strsep-with-clang-and-mingw
+    char *rv = *stringp;
+    if (rv) {
+        *stringp += strcspn(*stringp, delim);
+        if (**stringp)
+            *(*stringp)++ = '\0';
+        else
+            *stringp = 0; }
+    return rv;
+}
+
+int main(int argc, char *argv[])
+{
+    FILE *in;
+    char *temp;
+    char task[SIZE];
+
+    char *name;
+    int priority;
+    int burst;
+
+    in = fopen(argv[1],"r");
+    
+    while (fgets(task,SIZE,in) != NULL) {
+        temp = strdup(task);
+        name = strsep(&temp,",");
+        priority = atoi(strsep(&temp,","));
+        burst = atoi(strsep(&temp,","));
+
+        // add the task to the scheduler's list of tasks
+        add(name,priority,burst);
+
+        free(temp);
+    }
+
+    fclose(in);
+
+    // invoke the scheduler
+    schedule();
+
+    return 0;
+}
---
+++ schedule_fcfs.c
@@ -0,0 +1,40 @@
+// Add external files
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "schedulers.h"
+#include "task.h"
+#include "list.h"
+#include "cpu.h"
+struct node *head=NULL; //empty header
+int nextTid=0;
+
+
+
+Task *picknexttask(){
+  
+  return head->task; 
+}
+  
+
+void add(char* name, int priority, int burst){   //add task to list
+	Task *newTask = (Task *)malloc(sizeof(Task));
+	newTask->name= name;
+	newTask->tid = nextTid++;
+	newTask->priority = priority;
+	newTask->burst = burst;
+ 
+	insert(&head,newTask);
+}
+
+void schedule() // required by problem definition
+{
+  Task* current;
+  traverse(head); // check list exists
+  while(head != NULL){
+    current= picknexttask();
+  	run(current,current->burst);
+  	delete(&head,current);
+  }
+}
+
